fd2b0c47fca4ed16cdd77b569c636f3dc63dd169
Gosila <goshila.sadaf@therightsw.com>
2022-08-26 12:41:18 +0500

    first commit

---
+++ cointopay_direct_cc_second/vendor/cointopay/lib/Merchant/Order.php
@@ -0,0 +1,36 @@
+<?php
+namespace cointopay_direct_cc_second\Merchant;
+
+use cointopay_direct_cc_second\Cointopay_Direct_Cc_Second;
+use cointopay_direct_cc_second\Merchant;
+
+class Order extends Merchant
+{
+    private $order;
+
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    public static function createOrFail($params, $options = array(), $authentication = array())
+    {
+        $order = Cointopay_Direct_Cc_Second::request('orders', 'GET', $params, $authentication);
+        return new self($order);
+    }
+	public static function ValidateOrder($params, $options = array(), $authentication = array())
+    {
+        $order = Cointopay_Direct_Cc_Second::request('validation', 'GET', $params, $authentication);
+        return new self($order);
+    }
+
+    public function toHash()
+    {
+        return $this->order;
+    }
+
+    public function __get($name)
+    {
+        return $this->order[$name];
+    }
+}
